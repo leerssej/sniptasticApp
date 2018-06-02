c62655ce7902035cfa35b9edc17ab714131e637d
leerssej <jens.leerssen@gmail.com>
2018-06-02 10:29:07 -0700

    scrubbing down html

--- app.js
+++ app.js
@@ -14,25 +14,25 @@ const highlightCode = () => hljs.highlightBlock($('.display').get(0));
 
 $(document).ready(function () {
 
-  $('.setData').on('click', function() {
+  $('.setData').click(function() {
     let snippetKey = $('#nameInputField').val();
     let snippetValue = $('#codeInputField').val();
     localStorage.setItem(snippetKey, snippetValue);
     $('.nameField').val(''); // empty the NameField on save
   });
 
-  $('.getData').click( function () {
+  $('.getData').click(function() {
     cleanSlate();
     let desiredName = $('#nameInputField').val();
     let snippet = localStorage.getItem(desiredName);
     injectCode(snippet);
     highlightCode();
   });
-  
-  $('.getAll').click(function () { 
+
+  $('.getAll').click(function() { 
     cleanSlate();
     getAllCode().map(snippet => injectCode(snippet));
     highlightCode();
   });
-  
+
 });
