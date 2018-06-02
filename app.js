f8991f643b757ef0161c29f278eb9d04ca02a33b
leerssej <jens.leerssen@gmail.com>
2018-06-02 16:53:20 -0700

    data-id targetting improved

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const cleanSlate = () => {
   $('.sample').text('');
   $('.display').text('');
 };
-const injectCode = snippet => {
+const injectCode = (snippet, i = 0) => {
   $('.display')
-    .append(`<code>${snippet}</code>
+    .append(`<code data-id=${i}>${snippet}</code>
         <br>`);
 };
 const highlightCode = () => hljs.highlightBlock($('.display').get(0));
@@ -31,8 +31,29 @@ $(document).ready(function () {
 
   $('.getAll').click(function() { 
     cleanSlate();
-    getAllCode().map(snippet => injectCode(snippet));
+    getAllCode().map((snippet, i) => injectCode(snippet, i));
     highlightCode();
+  });
+
+  $('.viewContainer').click(function(e) { 
+    // e.preventDefault();
+    let targetDataId;
+    if (e.target.closest("code")) {
+      targetDataId = e.target.closest("code").attributes["0"].nodeValue;
+    }
+    // let parentTargetDataId = e.target.parentElement.attributes["0"].nodeValue;
+    // console.clear();
+    // console.dir(targetDataId)
+    // console.dir(parentTargetDataId)
+    // console.dir(e.target.parentElement)
+    // console.dir(e.target.closest("code"))
+    // console.dir(e.target.closest("code").attributes["0"].nodeValue);
+    // console.dir($(this).closest("code"))
+    if (targetDataId) {
+      console.dir(targetDataId)
+    } else {
+      console.log('please click on the code!')
+    }
   });
 
 });
