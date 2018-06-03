8d04b9942b58b37ca338e8c9e7d71a0e1d4d2934
leerssej <jens.leerssen@gmail.com>
2018-06-02 22:48:24 -0700

    field validation added

--- app.js
+++ app.js
@@ -11,14 +11,44 @@ const injectCode = (snippet, i = 0) => {
 };
 const highlightCode = () => hljs.highlightBlock($('.display').get(0));
 
+// // Show Alert
+// const showAlert = (message, className) => {
+//   // Create div
+//   const div = document.createElement('div');
+//   // Add classes
+//   div.className = `alert ${className}`;
+//   // Add text
+//   div.appendChild(document.createTextNode(message));
+//   // Get parent
+//   const container = document.querySelector('.container');
+//   // Get form
+//   const form = document.querySelector('#book-form');
+//   // Insert alert
+//   container.insertBefore(div, form);
+
+//   // Timeout after 3 sec
+//   setTimeout(function () {
+//     document.querySelector('.alert').remove();
+//   }, 3000);
+// }
+
 
 $(document).ready(function () {
 
   $('.setData').click(function() {
     let snippetKey = $('#nameInputField').val();
     let snippetValue = $('#codeInputField').val();
-    localStorage.setItem(snippetKey, snippetValue);
-    $('.nameField').val(''); // empty the NameField on save
+
+    // Validate
+    if (snippetKey === '' || snippetValue === '') {
+      // Error alert
+      console.log("empty!");
+      // ui.showAlert('Please fill in all fields', 'error');
+    } else {
+      // Add snippet to localStorage
+      localStorage.setItem(snippetKey, snippetValue);
+    }
+      $('.nameField').val(''); // empty the NameField on save
   });
 
   $('.getData').click(function() {
